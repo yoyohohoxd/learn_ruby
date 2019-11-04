9c09c31e2d771c7f2a52bedcfe61e1e4b3b3545a
yoyohohoxd <madse@live.dk>
2019-11-04 14:26:02 +0100

    First two exercises done

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc (a)
+    conversion = (a - 32.0) * 5.0 / 9.0
+end
+
+def ctof (a)
+    conversion = (a * 9.0 / 5.0) + 32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,40 @@
 #write your code here
+def add (a, b)
+    a + b
+end
+
+def subtract (a, b)
+    a - b
+end
+
+def sum(array)
+    sum = 0
+    array.each do |i|
+        sum += i
+    end
+    sum
+end
+
+def multiply(array)
+    sum = array[0]
+    array[1..-1].each do |i|
+        sum *= i
+    end
+    sum
+end
+
+def power(a, b)
+    a ** b
+end
+
+def factorial(a)
+    if a < 0
+      return nil
+    end
+    sum = 1
+    while a > 0
+      sum *= a
+      a -= 1
+    end
+    sum
+  end
