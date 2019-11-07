297182fcd77d2a0d1e7352298d8f075d23a9b7eb
yoyohohoxd <madse@live.dk>
2019-11-07 13:26:08 +0100

    Need to do pig latin. Could not figure it out.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,41 @@
 #write your code here
+def echo(string)
+    string
+end
+
+def shout(string)
+    string.upcase
+end
+
+def repeat(string, a = 2)
+    if a <= 2
+        string + ' ' + string
+    elsif a > 2
+        repeat_word = string + ' '
+        full_output = repeat_word * a
+        full_output.delete_suffix(' ')
+    end
+end
+
+def start_of_word(string, a = 1)
+    string[0, a]
+end
+
+def first_word(string)
+    string.split(' ')[0]
+end
+
+def titleize(string)
+    special_words = ['a', 'an', 'and', 'of', 'the', 'over']
+      fixed_words = []
+      string.downcase.split.each_with_index do |word, index|
+        # If this isn't the first word, and it's special, use downcase
+        if index > 0 and special_words.include?(word)
+          fixed_words << word
+        # It's either the first word, or not special, so capitalize
+        else
+          fixed_words << word.capitalize
+        end
+      end
+      fixed_words.join(" ")
+  end
